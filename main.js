cf19a76dd00d7db0f5fc640301e5c038d0e4e27e
Kartik Bhanderi <kartikbhanderi555@gmail.com>
2025-08-03 16:54:28 -0700

    fsdfd

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('navMenu');
+  const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("navLinks");
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
 
+    document.querySelectorAll(".nav-links a").forEach(link => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+      });
+    });
